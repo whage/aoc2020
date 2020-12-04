2dd648e9777b72670421b3baca9663eb9a11f8b1
whage <ndrs.sallai@gmail.com>
2020-12-04 20:53:33 +0100

    day 4: fixed line reading bug (last passports were not added because the last empty line wasnt handled), solved part 2 as well

--- 4/4.go
+++ 4/4.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"regexp"
 	"strings"
+	"strconv"
 )
+
+func atoi(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
 
 func main() {
 	file, _ := os.Open("passports.txt")
@@ -25,7 +32,32 @@ func main() {
 		}
 	}
 
+	if currentPassport != "" { passports = append(passports, strings.Trim(currentPassport, " ")) }
+
 	validCount := 0
+
+	rules := map[string]func(string)bool{
+		"byr": func(s string) bool { return atoi(s) >= 1920 && atoi(s) <= 2002 },
+		"iyr": func(s string) bool { return atoi(s) >= 2010 && atoi(s) <= 2020 },
+		"eyr": func(s string) bool { return atoi(s) >= 2020 && atoi(s) <= 2030 },
+		"hgt": func(s string) bool {
+			r := regexp.MustCompile(`(\d+)(cm|in)`)
+			matches := r.FindStringSubmatch(s)
+			if matches == nil { return false }
+			if matches[2] == "cm" { return atoi(matches[1]) >= 150 && atoi(matches[1]) <= 193 }
+			if matches[2] == "in" { return atoi(matches[1]) >= 59 && atoi(matches[1]) <= 76 }
+			return false
+		},
+		"hcl": func(s string) bool {
+			return regexp.MustCompile(`#([0-9a-fA-F]{6})`).FindStringSubmatch(s) != nil
+		},
+		"ecl": func(s string) bool {
+			return regexp.MustCompile(`(amb|blu|brn|gry|grn|hzl|oth)`).FindStringSubmatch(s) != nil
+		},
+		"pid": func(s string) bool {
+			return regexp.MustCompile(`^[0-9]{9}$`).FindStringSubmatch(s) != nil
+		},
+	}
 
 	for _, pp := range passports {
 		requiredSegments := map[string]bool{
@@ -36,22 +68,22 @@ func main() {
 			"hcl": false,
 			"ecl": false,
 			"pid": false,
-			//"cid": false,
 		}
 		foundAll := true
 		for _, seg := range strings.Split(pp, " ") {
-			requiredSegments[seg[:3]] = true
+			if seg[:3] == "cid" { continue }
+			ruleOk := rules[seg[:3]](seg[4:])
+			if ruleOk {
+				requiredSegments[seg[:3]] = true
+			}
 		}
-		fmt.Println(requiredSegments)
 		for _, reqSeg := range requiredSegments {
 			if !reqSeg { foundAll = false }
 		}
 		if foundAll {
-			fmt.Println("VALID")
 			validCount++
 		}
 	}
 
-	//fmt.Println(passports)
 	fmt.Println(validCount)
 }
