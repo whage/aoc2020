ba9095dae33d39158200c56728d62a2824ea59cc
András Sallai <ndrs.sallai@gmail.com>
2020-12-24 22:33:04 +0000

    day 17: done

--- 17_conway_cubes/17-1.go
+++ 17_conway_cubes/17-1.go
@@ -2,22 +2,79 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
-type Coord struct {
-	X, Y int
+type Pos struct {
+	X, Y, Z, W int
 }
 
-type Cell struct {
-	NeighborCount int
-	IsAlive bool
+type Grid map[Pos]bool
+
+type NeighborCountGrid map[Pos]int
+
+func readGrid(inputData []byte) Grid {
+	lines := strings.Split(string(inputData), "\n")
+
+	grid := Grid{}
+
+	for x := 0; x < len(lines[0]); x++ {
+		for y := 0; y < len(lines); y++ {
+			if lines[y][x] == '#' {
+				grid[Pos{x, y, 0, 0}] = true
+			}
+		}
+	}
+
+	return grid
+}
+
+func getNeighborCounts(grid Grid) NeighborCountGrid {
+	gridOfNeighborCounts := NeighborCountGrid{}
+	for pos, _ := range grid {
+		for dx := -1; dx < 2; dx++ {
+			for dy := -1; dy < 2; dy++ {
+				for dz := -1; dz < 2; dz++ {
+					for dw := -1; dw < 2; dw++ {
+						if dx == 0 && dy == 0 && dz == 0 && dw == 0 { continue }
+						targetPos := Pos{pos.X+dx, pos.Y+dy, pos.Z+dz, pos.W+dw}
+						gridOfNeighborCounts[targetPos] += 1
+					}
+				}
+			}
+		}
+	}
+	return gridOfNeighborCounts
+}
+
+func step(grid Grid, counter NeighborCountGrid) Grid {
+	newGrid := Grid{}
+	for pos, neighborCount := range counter {
+		_, isAlive := grid[pos]
+		if isAlive {
+			if neighborCount == 2 || neighborCount == 3 {
+				newGrid[pos] = true
+			}
+		} else {
+			if neighborCount == 3 {
+				newGrid[pos] = true
+			}
+		}
+	}
+	return newGrid
 }
 
 func main() {
-	m := map[Coord]int{
-		Coord{1, 1}: 1,
-		Coord{2, 1}: 2,
-		Coord{3, 1}: 1,
+	data, _ := ioutil.ReadFile("data-1.txt")
+
+	grid := readGrid(data)
+	stepCount := 1
+
+	for stepCount <= 6 {
+		grid = step(grid, getNeighborCounts(grid))
+		stepCount++
 	}
-	fmt.Println(m)
+
+	fmt.Println(len(grid))
 }
